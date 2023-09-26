9fd002cf933133d3d0bb9f5e00832d90a303658e
Shivani-Banduni <shivanibanduni009@gmail.com>
2023-09-27 00:41:41 +0530

    Difference between promise and async await through code

--- index.js
+++ index.js
@@ -103,11 +103,102 @@
 // const member = new Person('Lydia', 'Hallie');
 // console.log(member)
 
+// Usestrict stops to behave like regular expressions.
 
-function getAge() {
-  'use strict';
-  age = 21;
-  console.log(age);
+// function getAge() {
+//    'use strict';
+//   age = 21;
+//   console.log(age);
+// }
+
+// getAge();
+
+
+
+// function main(){
+//   console.log('start')
+// function promise(){
+//   return new Promise((resolve,reject)=>{
+//     console.log('hi')
+// setTimeout(()=>{
+// resolve('Hurray Promise')
+// },2000)
+
+//   })
+// }
+// promise().then((res)=>{
+//   console.log(res)
+//   console.log(res)
+// }).catch((err)=>{
+//   console.log(err)
+// })
+// console.log('end promise')
+// }
+// main()
+// const id=new Promise (setTimeout(() => {
+//   console.log('await1',id)
+// }, 10000));
+// const id2= new Promise(setTimeout(()=>{
+//   console.log('waada timeout2',id2)
+// },1000))
+
+
+
+
+
+
+// Async Function
+
+const wadaa=new Promise((resolve,reject)=>{
+  setInterval(()=>{
+    resolve('success ho gya')
+  },1000)
+})
+const wada=new Promise((resolve,reject)=>{
+  console.log('first')
+  setInterval(()=>{
+    resolve('success')
+  },5000)
+})
+
+
+async function main(){
+  console.log('second request')
+
+  const id2=await wada // wait here untill executed
+console.log(id2)
+console.log('third request')
+const id=await wadaa;
+console.log(id)
+
+console.log('last')
 }
+main()
 
-getAge();
+
+
+// Using Promise
+
+// const promise=new Promise((resolve,reject)=>{
+//   console.log('promise2')
+// setTimeout(()=>{
+// resolve('success')
+// },4000)
+// })
+
+
+// const promise2=new Promise((resolve,reject)=>{
+//   setTimeout(()=>{
+//   resolve('success2')
+//   },1000)
+//   })
+//   function main(){
+//     console.log('main1')
+//     promise2.then((res)=>{
+//       console.log(res)
+//     })
+// promise.then((res)=>{
+//   console.log(res)
+// })
+
+//   }main()
